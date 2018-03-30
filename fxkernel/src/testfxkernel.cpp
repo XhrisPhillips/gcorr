2f61980098038d3d8299371213886d665ce6c8a6
Adam Deller <adeller@gmail.com>
2018-03-30 16:30:20 +1100

    Added an automake setup and a test harness for FxKernel

---
+++ fxkernel/src/testfxkernel.cpp
@@ -0,0 +1,37 @@
+#include "fxkernel.h"
+
+int main(int argc, char *argv[])
+{
+  // variables for the test
+  char ** inputdata;
+  double ** delays;
+  int numchannels, numantennas, numffts;
+  double lo, bandwidth;
+
+  // load up the test input data from somewhere
+  
+  // Load up the delays from somewhere
+
+  // Set the inputs we'll use as a test
+  // Current values would give a subint of 100ms, which is fairly reasonable
+  numchannels = 1024;
+  numantennas = 6;
+  numffts = 3125;
+  lo = 1650000000.0;
+  bandwidth = 32000000.0;
+
+  // create the FxKernel
+  FxKernel fxkernel = FxKernel(numchannels, numatennas, numffts, lo, bandwidth);
+
+  // Set the input data and the delays
+  fxkernel.setInputData(inputdata);
+  fxkernel.setDelays(delays);
+
+  // Checkpoint for timing
+  
+  // Run the processing
+  fxkernel.process();
+
+  // Calculate the elapsed time
+  
+}
