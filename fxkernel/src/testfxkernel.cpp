0cda3285414bc53a7b3b92b0bff162517f43d96a
Chis Phillips <Chris.Phillips@csiro.au>
2018-04-10 12:40:17 +1000

    Parse config file

--- fxkernel/src/testfxkernel.cpp
+++ fxkernel/src/testfxkernel.cpp
@@ -1,4 +1,14 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <strings.h>
+#include <string.h>
+
+using std::string;
+using std::cout;
+using std::cerr;
+using std::endl;
 
 #include "fxkernel.h"
 #include "vectordefs.h"
@@ -19,8 +29,8 @@ void allocData(u8 ***data, double *** delays, int numantenna, int numchannels, i
   }
   
   int bytespersubint = numchannels*cfactor*numffts*nbit/8*nPol;
-  std::cout << "Allocating " << bytespersubint/1024/1024 << " MB per antenna per subint" << std::endl;
-  std::cout << "          " << bytespersubint * numantenna * nsubint / 1024 / 1024 << " MB total" << std::endl;
+  cout << "Allocating " << bytespersubint/1024/1024 << " MB per antenna per subint" << endl;
+  cout << "          " << bytespersubint * numantenna * nsubint / 1024 / 1024 << " MB total" << endl;
 
 
   *data = new u8*[numantenna];
@@ -32,24 +42,95 @@ void allocData(u8 ***data, double *** delays, int numantenna, int numchannels, i
   }
 }
 
+void parseConfig(char *config, int &nbit, bool &iscomplex, int &nchan, int &nant, double &lo, double &bandwidth,
+		 char ***antenna, char ***antFiles ) {
+
+  std::ifstream fconfig(config);
+
+  string line;
+  int anttoread = 0;
+  int iant = 0;
+  while (std::getline(fconfig, line)) {
+    std::istringstream iss(line);
+    string keyword;
+    if (!(iss >> keyword)) {
+      cerr << "Error: Could not parse \"" << line << "\"" << endl;
+      exit(1);
+    }
+    if (anttoread) {
+      (*antenna)[iant] = new char[keyword.length()+1];
+      strcpy((*antenna)[iant],keyword.c_str());
+      string thisfile;
+      iss >> thisfile;
+      (*antFiles)[iant] = new char[thisfile.length()+1];
+      strcpy((*antFiles)[iant],thisfile.c_str());
+      iant++;
+      anttoread--;
+    } else if (strcasecmp(keyword.c_str(), "COMPLEX")==0) {
+      iss >> iscomplex; // Should error check
+    } else if (strcasecmp(keyword.c_str(), "NBIT")==0) {
+      iss >> nbit; // Should error check
+    } else if (strcasecmp(keyword.c_str(), "NCHAN")==0) {
+      iss >> nchan; // Should error check
+    } else if (strcasecmp(keyword.c_str(), "LO")==0) {
+      iss >> lo; // Should error check
+    } else if (strcasecmp(keyword.c_str(), "BANDWIDTH")==0) {
+      iss >> bandwidth; // Should error check
+    } else if (strcasecmp(keyword.c_str(), "NANT")==0) {
+      iss >> nant; // Should error check
+      *antenna = new char*[nant];
+      *antFiles = new char*[nant];
+      anttoread = nant;
+      iant = 0;
+    } else {
+      std::cerr << "Error: Unknown keyword \"" << keyword << "\"" << endl;
+    }
+  }
+}
+
 
 int main(int argc, char *argv[])
 {
   // variables for the test
+  char *configfile;
   int i;
   u8 ** inputdata;
   double ** delays;
   int numchannels, numantennas, numffts, nbit, nsubint;
   double lo, bandwidth;
+  bool iscomplex;
+  char **antennas, **antFiles;
 
+  if (argc!=2) {
+    cout << "Usage:  testfxkernel <config>\n" << endl;
+    exit(1);
+  }
+
+  configfile = argv[1];
+
+  parseConfig(configfile, nbit, iscomplex, numchannels, numantennas, lo, bandwidth, &antennas, &antFiles);
+
+  cout << "Got COMPLEX " << iscomplex << endl;
+  cout << "Got NBIT " << nbit << endl;
+  cout << "Got NCHAN " << numchannels << endl;
+  cout << "Got LO " << lo << endl;
+  cout << "Got BANDWIDTH" << bandwidth << endl;
+  cout << "Got NANT " << numantennas << endl;
+  for (int i=0;i<numantennas;i++) {
+    cout << "  " << antennas[i] << ":" << antFiles[i] << endl;
+  }
+    
+
+  
+  exit(1);
   // Set the inputs we'll use as a test
   // Current values would give a subint of 100ms, which is fairly reasonable
-  nbit = 2;
-  numchannels = 1024;
-  numantennas = 6;
+  //nbit = 2;
+  //numchannels = 1024;
+  //numantennas = 6;
   numffts = 3125;
-  lo = 1650000000.0;
-  bandwidth = 32000000.0;
+  //lo = 1650000000.0;
+  //bandwidth = 32000000.0;
   nsubint = 10;
 
   // Allocate space in the buffers for the data and the delays
