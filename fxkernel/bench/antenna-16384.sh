56cbe1d075d82595a4b2f5a1a508df33f95d8dc0
Chis Phillips <Chris.Phillips@csiro.au>
2018-11-05 21:39:03 +1100

    Added 16K channel test

---
+++ fxkernel/bench/antenna-16384.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NTHREAD=<setme>
+NCHAN=16384
+NLOOP=1
+
+nant=(4 6 8 10 12 16)
+
+for a in ${nant[@]}; do
+    echo  -n "NANT= $a"
+    ../src/bench_fxkernel -n $NLOOP -t $NTHREAD test${a}-16384.conf | grep Mbps
+done
