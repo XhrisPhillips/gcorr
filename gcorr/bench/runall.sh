4a956de77579e8730e0a62f8ef79248013f2c45c
Chis Phillips <Chris.Phillips@csiro.au>
2018-11-09 23:05:55 +1100

    Run all tests

---
+++ gcorr/bench/runall.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -f
+
+echo "***CHANNELS***"
+channels.sh
+echo "***ANTENNA***"
+antenna.sh
+echo "***ANTENNA 16384***"
+antenna-16384.sh
+echo "***CHANNELS 8bit***"
+channels-8.sh
+echo "***ANTENNA 8bit***"
+antenna-8bit.sh
+echo "***ANTENNA 16384***"
+antenna-16384-8bit.sh
+
+echo "*****************"
+echo "******HALF*******"
+echo "*****************"
+
+echo "***CHANNELS***"
+channels-half.sh
+echo "***ANTENNA***"
+antenna-half.sh
+echo "***ANTENNA 16384***"
+antenna-16384-half.sh
+echo "***CHANNELS 8bit***"
+channels-8half.sh
+echo "***ANTENNA 8bit***"
+antenna-8bit-half.sh
+echo "***ANTENNA 16384***"
+antenna-16384-8bit-half.sh
+
