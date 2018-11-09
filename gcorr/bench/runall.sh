d39568bc5492fe9a4781d870505a6c8fb0631893
Chris Phillips <Chris.Phillips@csiro.au>
2018-11-09 23:10:11 +1100

    local files

--- gcorr/bench/runall.sh
+++ gcorr/bench/runall.sh
@@ -1,32 +1,32 @@
 #!/bin/bash -f
 
 echo "***CHANNELS***"
-channels.sh
+./channels.sh
 echo "***ANTENNA***"
-antenna.sh
+./antenna.sh
 echo "***ANTENNA 16384***"
-antenna-16384.sh
+./antenna-16384.sh
 echo "***CHANNELS 8bit***"
-channels-8.sh
+./channels-8.sh
 echo "***ANTENNA 8bit***"
-antenna-8bit.sh
+./antenna-8bit.sh
 echo "***ANTENNA 16384***"
-antenna-16384-8bit.sh
+./antenna-16384-8bit.sh
 
 echo "*****************"
 echo "******HALF*******"
 echo "*****************"
 
 echo "***CHANNELS***"
-channels-half.sh
+./channels-half.sh
 echo "***ANTENNA***"
-antenna-half.sh
+./antenna-half.sh
 echo "***ANTENNA 16384***"
-antenna-16384-half.sh
+./antenna-16384-half.sh
 echo "***CHANNELS 8bit***"
-channels-8half.sh
+./channels-8half.sh
 echo "***ANTENNA 8bit***"
-antenna-8bit-half.sh
+./antenna-8bit-half.sh
 echo "***ANTENNA 16384***"
-antenna-16384-8bit-half.sh
+./antenna-16384-8bit-half.sh
 
