7ce3d1fe4518d1000aa11cba1a9cf00e0732f7af
Xinping Deng <den15c@csiro.au>
2019-09-20 11:42:01 +1000

    upload to use the code at mac

--- network_bench/src/setitimer_test.c
+++ network_bench/src/setitimer_test.c
@@ -5,7 +5,7 @@
 #include <time.h>
 #include <unistd.h>
 
-// gcc setitimer_test.c -o settimer_test -lrt
+// gcc setitimer_test.c -o setitimer_test -lrt
 void missed_alarm(int signum) {
   /* we missed a timer signal, so won't be sending packets fast enough!!! */
   write(2, "Missed Alarm!\n", 14); /* can't use printf in a signal handler */
@@ -29,14 +29,14 @@ int main(int argc, char *argv[])
   sigemptyset(&alarm_sig);
   sigaddset(&alarm_sig, SIGALRM);
   while (1) {
-    //clock_gettime(CLOCK_BOOTTIME, &start); // Timer at the beginning
+    clock_gettime(CLOCK_BOOTTIME, &start); // Timer at the beginning
     
     signal(SIGALRM, missed_alarm);
     sigwait(&alarm_sig, &signum); /* wait until the next signal */
 
-    //clock_gettime(CLOCK_BOOTTIME, &stop);  // Timer at the end
-    //elapsed_time = (stop.tv_sec - start.tv_sec) + (stop.tv_nsec - start.tv_nsec)/1.0E9L;
-    //fprintf(stdout, "%E seconds\n", elapsed_time);
+    clock_gettime(CLOCK_BOOTTIME, &stop);  // Timer at the end
+    elapsed_time = (stop.tv_sec - start.tv_sec) + (stop.tv_nsec - start.tv_nsec)/1.0E9L;
+    fprintf(stdout, "%E seconds\n", elapsed_time);
   }
 
   return EXIT_SUCCESS;
