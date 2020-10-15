b20ea43d991e99b8fd587715732c9259af19c106
Xinping Deng <den15c@csiro.au>
2020-10-15 14:11:53 +1100

    more

--- nic_trial/client.py
+++ nic_trial/client.py
@@ -6,9 +6,9 @@ import socket
 client = iperf3.Client()
 client.duration = 1
 if socket.gethostname() == "frigg":
-    client.server_hostname = "10.17.4.1"
-if socket.gethostname() == "odin":
     client.server_hostname = "10.17.4.2"
+if socket.gethostname() == "odin":
+    client.server_hostname = "10.17.4.1"
 client.port = 5201
 client.protocol = 'udp'
 
